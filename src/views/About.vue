80af54615bade904d73925b7344ff8722891f315
Yaroslav Georgitsa <yaroslav.georgitsa@gmail.com>
2019-07-21 18:27:34 +0300

    added select option

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,4 +1,16 @@
 <template lang="pug">
   .about
-    h1 This is an about page
+      HelloWorld(msg="Vue.js + TypeScript App")
 </template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+
+@Component({
+  components: {
+    HelloWorld
+  }
+})
+export default class Home extends Vue {}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,37 @@
 <template lang="pug">
   .home
-    HelloWorld(msg="Welcome to Your Vue.js + TypeScript App")
+    div
+      select(@change="onSelectPossibleOption")
+        option(
+          v-for="option in possibleOptions"
+          :disabled="selectedOptions.includes(option)"
+        ) {{option}}
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+  import {Component, Vue} from "vue-property-decorator";
 
-@Component({
-  components: {
-    HelloWorld
+  @Component({
+    components: {}
+  })
+  export default class Home extends Vue {
+    possibleOptions: Array<string> = [
+      'side1',
+      'side2',
+      'side3',
+      'angleOppositeToSide1',
+      'angleOppositeToSide2',
+      'angleOppositeToSide3',
+    ]
+    selectedOptions: Array<string> = []
+
+
+    onSelectPossibleOption(event: Event): void {
+      const value = (<HTMLInputElement>event.currentTarget).value;
+      (<HTMLInputElement>event.currentTarget).value = '';
+      if (!this.selectedOptions.includes(value)) {
+        this.selectedOptions.push(value)
+      }
+    }
   }
-})
-export default class Home extends Vue {}
 </script>
